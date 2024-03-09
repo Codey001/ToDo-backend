b3079c391b23c836105252bd9afb00b4c72b49b6
Sunny Kumar Yadav <97532891+Codey001@users.noreply.github.com>
2024-03-09 15:02:01 +0530

    initial backend files uploaded

---
+++ models/ToDoModel.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const toDoSchema = new mongoose.Schema({
+  text: { type: String, require: true },
+});
+
+module.exports = mongoose.model("ToDo", toDoSchema);
